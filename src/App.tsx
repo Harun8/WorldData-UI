a03ba2b71db8ea6fe69236fe395e1ff9fb610ee3
Harun8 <harunabdi8@gmail.com>
2025-01-25 12:53:21 +0100

    inital chart add + styling fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { NumberTicker } from './components/ui/number-ticker'
+import { DotPattern } from './components/ui/dot-pattern'
+import { cn } from './lib/utils'
+import { SparkAreaExample } from './components/table'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<Number>(0)
+
+
+  useEffect(()=> {
+
+    const getPopulationTable = async() => {
+
+    }
+  })
 
   return (
     <>
-      <div>
-       <NumberTicker
-       value={100}
-       className='whitespace-pre-wrap text-8xl font-medium tracking-tighter text-black dark:text-white'
+      <div className="flex flex-col items-center h-screen w-screen  overflow-hidden rounded-lg border bg-background p-20 md:shadow-xl">
+      <h1 className=" text-4xl md:text-7xl font-bold font-sans mb-4 font-extrabold	 text-sky-800 ">
+        Data from {" "}
+         <NumberTicker
+       value={265}
+       className='whitespace-pre-wrap text-5xl font-medium tracking-tighter font-extrabold  text-sky-800  dark:text-white'
        ></NumberTicker>
-      </div>
+          <span> {" "} places around the world</span>
+        </h1>
+
+      <DotPattern 
+        width={20}
+        height={20}
+        cx={1}
+        cy={1}
+        cr={1}
+        className={cn(
+          "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]",
+        )}
+      />
+
+<SparkAreaExample></SparkAreaExample>
+    </div>
+      {/* <div className="flex flex-col items-center mt-24 text-center min-h-screen">
+
+      <h1 className=" text-4xl md:text-7xl font-bold font-sans mb-4 font-extrabold	 text-sky-800 ">
+        Data from {" "}
+         <NumberTicker
+       value={265}
+       className='whitespace-pre-wrap text-5xl font-medium tracking-tighter font-extrabold  text-sky-800  dark:text-white'
+       ></NumberTicker>
+          <span> {" "} places around the world</span>
+        </h1>
+      
+      </div> */}
     </>
   )
 }
